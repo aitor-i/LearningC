9d0c36415fed6c7746a6da9faf304fbd56c574bb
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-09-05 15:58:02 +0200

    ADD - History slim view model

---
+++ API_parking_bicis/ViewModels/HistorySlimViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace API_parking_bicis.ViewModels
+{
+    public class HistorySlimViewModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string ParkingName { get; set; } = string.Empty;
+
+        public HistorySlimViewModel()
+        {
+        }
+    }
+}
