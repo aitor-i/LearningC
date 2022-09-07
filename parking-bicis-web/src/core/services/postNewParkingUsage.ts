8b26d5bbd09e7f0ed693be0ca9452c9551c56d86
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-09-07 16:12:01 +0200

    ADD - Post new parking usage

---
+++ parking-bicis-web/src/core/services/postNewParkingUsage.ts
@@ -0,0 +1,11 @@
+import { parkingForm } from "../domain/type/parkingForm";
+
+export const postNewParkingUsage = async (usageForm: parkingForm) => {
+  return await fetch("http://localhost:6168/History/NewParkingUsage", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(usageForm),
+  });
+};
