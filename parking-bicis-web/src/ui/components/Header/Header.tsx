f302fd83e353dd4b6b845c38f8f393ba25904f9a
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-09-11 18:35:47 +0200

    UPDATE - Header

--- parking-bicis-web/src/ui/components/Header/Header.tsx
+++ parking-bicis-web/src/ui/components/Header/Header.tsx
@@ -2,15 +2,24 @@ import React, { useContext } from "react";
 import { LoginContext } from "../../store/loging-ctx";
 
 export const Header = () => {
-  const { isLogged, logOutHandler, loginHandler } = useContext(LoginContext);
+  const { isLogged, logOutHandler, userID } = useContext(LoginContext);
   return (
-    <div style={{ display: "flex", justifyContent: "space-between" }}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
       <h1>Parking</h1>
       {isLogged ? (
-        <button onClick={logOutHandler}>Log out</button>
-      ) : (
-        <button onClick={loginHandler}>Login</button>
-      )}
+        <>
+          <p style={{ color: "white" }}>Your id is: {userID}</p>
+          <button style={{ padding: "1rem" }} onClick={logOutHandler}>
+            Log out
+          </button>
+        </>
+      ) : null}
     </div>
   );
 };
