cca69255a3422c130f3007adf89d289e0bfeae24
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-09-14 11:39:23 +0200

    ADD - Generic repository

---
+++ Data_Parking_Bicis/Repository/GenericRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Data_Parking_Bicis.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data_Parking_Bicis.Repository
+{
+	public class GenericRepository<T>:IGenericRepository<T> where T : EntityBase
+	{
+        protected readonly DbContext _ctx;
+		public GenericRepository(DbContext ctx)
+		{
+            _ctx = ctx;
+		}
+
+        public async Task<IEnumerable<T>> GetValues()
+        {
+            return await _ctx.Set<T>().ToListAsync();
+        }
+
+        public async Task<int> Insert(T entity)
+        {
+            await _ctx.Set<T>().AddAsync(entity);
+            await _ctx.SaveChangesAsync();
+            return entity.Id;
+        }
+    }
+}
+
---
+++ Data_Parking_Bicis/Repository/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Data_Parking_Bicis.Models;
+
+namespace Data_Parking_Bicis.Repository
+{
+	public interface IGenericRepository<T> where T : EntityBase
+	{
+		Task<IEnumerable<T>> GetValues();
+		Task<int> Insert(T entity);
+	}
+}
+
