c810dcb440ec8c67cd4ce73f0879d8da8c6bf31e
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-09-14 11:42:12 +0200

    ADD - Login model

---
+++ Data_Parking_Bicis/Models/LoginModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Application_Parking_Bicis.ViewModels
+{
+    public class LoginModel
+	{
+		public string Username { get; set; } = string.Empty;
+		public string Password { get; set; } = string.Empty;
+		
+	}
+}
+
