bd8ac6f7b86e130bf05e047b1ef2601f44ee6c6c
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-09-14 14:53:28 +0200

    ADD - Unit Of Work

---
+++ Data_Parking_Bicis/uow/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Data_Parking_Bicis.Repository;
+
+namespace Data_Parking_Bicis.UOW
+{
+	public interface IUnitOfWork
+	{
+        IUserRepository UserRepository { get; }
+        Task CompleteAsync();
+    }
+}
+
---
+++ Data_Parking_Bicis/uow/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Data_Parking_Bicis.data;
+using Data_Parking_Bicis.Repository;
+using Data_Parking_Bicis.UOW;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data_Parking_Bicis.UOW
+{
+	public class UnitOfWork: IUnitOfWork, IDisposable
+	{
+        public IUserRepository UserRepository { get; private set; }
+        private readonly DataContext _ctx;
+		public UnitOfWork()
+		{
+
+		}
+
+        public UnitOfWork(DataContext ctx)
+        {
+            UserRepository = new UserRepository(ctx);
+            _ctx = ctx;
+        }
+
+        public async Task CompleteAsync()
+        {
+            await _ctx.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _ctx.Dispose();
+        }
+    }
+}
+
