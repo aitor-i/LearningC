2baa8650a95ce1a0663a5a2470e0f5850d585b2b
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-09-20 13:15:58 +0200

    REFACTOR - Register new user

--- parking-bicis-web/src/ui/components/RegisterForm/RegisterForm.tsx
+++ parking-bicis-web/src/ui/components/RegisterForm/RegisterForm.tsx
@@ -1,46 +1,15 @@
-import React, { ChangeEvent, useState } from "react";
-import { RegisterType } from "../../../core/domain/type/RegisterType";
-import { postRegisterNewUser } from "../../../core/services/postRegisterNewUser";
 import Spinner from "../Spinner";
+import { useRegisterUser } from "./useRegisterUser";
 
 export const RegisterForm = () => {
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  const [userType, setUserType] = useState(1);
-  const [loadingState, setLoadingState] = useState<
-    "idle" | "loading" | "success" | "error"
-  >("idle");
-  const registerRef = React.useRef<HTMLFormElement>(null);
-  const nameHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    setName(event.target.value);
-  };
-  const passwordHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value);
-  };
-  const selectParkingHandler = (event: ChangeEvent<HTMLSelectElement>) => {
-    setUserType(+event.target.value);
-  };
-  const submitHandler = async (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    event.preventDefault();
-    setLoadingState("loading");
-    try {
-      const registerForm: RegisterType = {
-        password,
-        username: name,
-        userTypeId: userType,
-      };
-      const response = await postRegisterNewUser(registerForm);
-      alert(`Your id is ${response}`);
-      setLoadingState("success");
-    } catch (error) {
-      alert(error);
-      setLoadingState("error");
-    } finally {
-      if (registerRef.current) registerRef.current.reset();
-    }
-  };
+  const {
+    loadingState,
+    nameHandler,
+    passwordHandler,
+    selectParkingHandler,
+    submitHandler,
+    registerRef,
+  } = useRegisterUser();
 
   return (
     <form ref={registerRef}>
---
+++ parking-bicis-web/src/ui/components/RegisterForm/useRegisterUser.tsx
@@ -0,0 +1,51 @@
+import React, { ChangeEvent, useState } from "react";
+import { RegisterType } from "../../../core/domain/type/RegisterType";
+import { postRegisterNewUser } from "../../../core/services/postRegisterNewUser";
+
+export const useRegisterUser = () => {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [userType, setUserType] = useState(1);
+  const [loadingState, setLoadingState] = useState<
+    "idle" | "loading" | "success" | "error"
+  >("idle");
+  const registerRef = React.useRef<HTMLFormElement>(null);
+  const nameHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+  const passwordHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+  const selectParkingHandler = (event: ChangeEvent<HTMLSelectElement>) => {
+    setUserType(+event.target.value);
+  };
+  const submitHandler = async (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+    setLoadingState("loading");
+    try {
+      const registerForm: RegisterType = {
+        password,
+        username: name,
+        userTypeId: userType,
+      };
+      const response = await postRegisterNewUser(registerForm);
+      alert(`Your id is ${response}`);
+      setLoadingState("success");
+    } catch (error) {
+      alert(error);
+      setLoadingState("error");
+    } finally {
+      if (registerRef.current) registerRef.current.reset();
+    }
+  };
+  return {
+    loadingState,
+    nameHandler,
+    passwordHandler,
+    selectParkingHandler,
+    submitHandler,
+    registerRef,
+  };
+};
