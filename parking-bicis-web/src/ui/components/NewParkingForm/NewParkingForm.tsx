7d608ea5c3f81924038a8a3867eddce29e7c54c5
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-09-27 08:35:43 +0200

    REFACTOR - new parking hook

--- parking-bicis-web/src/ui/components/NewParkingForm/NewParkingForm.tsx
+++ parking-bicis-web/src/ui/components/NewParkingForm/NewParkingForm.tsx
@@ -2,35 +2,10 @@ import React, { useContext, useState } from "react";
 import { postNewParking } from "../../../core/services/postNewParking";
 import { LoginContext } from "../../store/loging-ctx";
 import Spinner from "../Spinner";
+import { useNewParking } from "./useNewParking";
 
 export const NewParkingForm = () => {
-  const [parkingName, setParkingName] = useState("");
-  const [loadingState, setLoadingState] = useState<
-    "idle" | "loading" | "success" | "error"
-  >("idle");
-  const { user } = useContext(LoginContext);
-  const parkingNameHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setParkingName(event.target.value);
-  };
-
-  const publishHandler = async (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    event.preventDefault();
-    try {
-      setLoadingState("loading");
-      const res: number = await postNewParking({
-        usersId: user.usersId,
-        parkinName: parkingName,
-      });
-
-      alert(`Parking published, parking id: ${res}`);
-      setLoadingState("success");
-    } catch (error) {
-      alert(error);
-      setLoadingState("error");
-    }
-  };
+  const { loadingState, parkingNameHandler, publishHandler } = useNewParking();
 
   return (
     <form>
