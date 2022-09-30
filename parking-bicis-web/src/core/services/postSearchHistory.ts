299360c0840a4f131d253a72a4544583b9cc3dda
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-09-30 14:06:35 +0200

    UPDATE - search history

--- parking-bicis-web/src/core/services/postSearchHistory.ts
+++ parking-bicis-web/src/core/services/postSearchHistory.ts
@@ -1,10 +1,9 @@
 export const postSearchHistory = async (searchParam: string) => {
-  const response = fetch(
-    `${process.env.REACT_APP_API_URL}/History/search?expression=${searchParam}`,
-    {
-      method: "POST",
-    }
-  );
+  const endpoint = new URL(`${process.env.REACT_APP_API_URL}/History/search`);
+  endpoint.searchParams.append("expression", searchParam);
+  const response = fetch(endpoint, {
+    method: "POST",
+  });
 
   if (!(await response).ok) throw new Error((await response).toString());
   return (await response).json();
