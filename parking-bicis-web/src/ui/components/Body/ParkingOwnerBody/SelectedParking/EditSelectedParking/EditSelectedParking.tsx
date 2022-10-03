16a6945f2aff93d80ccdbe85933ce676f1e39578
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-10-03 16:57:50 +0200

    ADD - Styles edit parking name

--- parking-bicis-web/src/ui/components/Body/ParkingOwnerBody/SelectedParking/EditSelectedParking/EditSelectedParking.tsx
+++ parking-bicis-web/src/ui/components/Body/ParkingOwnerBody/SelectedParking/EditSelectedParking/EditSelectedParking.tsx
@@ -2,6 +2,8 @@ import { Parkings } from "../../../../../../core/domain/type/Parkings";
 import Spinner from "../../../../Spinner";
 import { useEditSelectedParking } from "./useEditSelectedParking";
 
+import "./edit-parking-name.css";
+
 interface Props {
   selectedParking: Parkings | undefined;
   changeToViewMode: () => void;
@@ -11,8 +13,13 @@ export const EditSelectedParking = ({
   selectedParking,
   changeToViewMode,
 }: Props) => {
-  const { editNameHandler, onSaveHandler, newParkingName, loadingStatus } =
-    useEditSelectedParking(changeToViewMode, selectedParking!);
+  const {
+    editNameHandler,
+    onSaveHandler,
+    newParkingName,
+    loadingStatus,
+    onCancelHandler,
+  } = useEditSelectedParking(changeToViewMode, selectedParking!);
 
   return (
     <form className="parking-data" onSubmit={onSaveHandler}>
@@ -24,7 +31,18 @@ export const EditSelectedParking = ({
         value={newParkingName}
       />
       <p>{selectedParking?.username}</p>
-      <button>{loadingStatus === "loading" ? <Spinner /> : "Save"}</button>
+      <div className="button-container">
+        {loadingStatus === "loading" ? (
+          <Spinner />
+        ) : (
+          <>
+            <button>Save</button>
+            <button className="cancel" onClick={onCancelHandler}>
+              Cancel
+            </button>
+          </>
+        )}
+      </div>
     </form>
   );
 };
