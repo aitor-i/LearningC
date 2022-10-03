ad94328d81192e04c32f000c20040dd0e9284fa3
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-10-03 16:56:51 +0200

    ADD - Refresh icon

--- parking-bicis-web/src/ui/components/Body/ParkingOwnerBody/ParkingList/ParkingList.tsx
+++ parking-bicis-web/src/ui/components/Body/ParkingOwnerBody/ParkingList/ParkingList.tsx
@@ -1,6 +1,9 @@
 import { Parkings } from "../../../../../core/domain/type/Parkings";
 import Spinner from "../../../Spinner";
 import { useParkin } from "../useParking";
+
+import { VscRefresh } from "react-icons/vsc";
+
 import "./parking-list.css";
 
 interface Props {
@@ -14,7 +17,9 @@ export const ParkingList = ({ selectParkingHandler }: Props) => {
     <div className="parkings-list">
       <div style={{ display: "flex", justifyContent: "space-between" }}>
         <h3>Parkings</h3>
-        <p onClick={fetchParkings}>R</p>
+        <p onClick={fetchParkings}>
+          <VscRefresh />
+        </p>
       </div>
       {fetchingStatus !== "loading" && fetchingStatus === "success" ? (
         parkings.map((parkin) => {
