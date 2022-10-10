17f91c1fd8f7d686e4c8ec1f7f34591baa984aed
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-10-10 21:04:06 +0200

    TEST - should feel input item

--- parking-bicis-web/src/ui/components/SearchCompnent/__test__/searchComponent.test.js
+++ parking-bicis-web/src/ui/components/SearchCompnent/__test__/searchComponent.test.js
@@ -1,5 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { faker } from "@faker-js/faker";
+
 import { SearchComponent } from "../SearchComponent";
+import userEvent from "@testing-library/user-event";
 
 describe("Search component", () => {
   it("should render search component", () => {
@@ -12,5 +15,16 @@ describe("Search component", () => {
     const searchButton = screen.getByRole("button", { name: /search/i });
 
     expect(searchButton.textContent).toMatch(/search/i);
+  });
+
+  it("should feel input item", () => {
+    render(<SearchComponent />);
+
+    const randomName = faker.name.firstName();
+    const input = screen.getByRole("textbox");
+
+    userEvent.type(input, randomName);
+
+    expect(input.value).toBe(randomName);
   });
 });
