9fb3d066bfaed9e8e24dca1462c318290d83cff7
Aitor Ibarra <ibarraaitor@protonmail.com>
2022-10-13 23:23:12 +0200

    TEST - should show refresh button

--- parking-bicis-web/src/ui/components/History/__test__/history.test.js
+++ parking-bicis-web/src/ui/components/History/__test__/history.test.js
@@ -1,8 +1,16 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { History } from "../History";
 
 describe("History component", () => {
   it("should render history component ", () => {
     render(<History />);
+  });
+
+  it("should show refresh button", () => {
+    render(<History />);
+
+    const button = screen.getByRole("button", { name: /refresh/i });
+
+    expect(button.textContent).toBe("Refresh");
   });
 });
